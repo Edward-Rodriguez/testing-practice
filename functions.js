701412406cef070dcf1219052e92bcb7e6e1ef70
Edward Rodriguez <36542201+Edward-Rodriguez@users.noreply.github.com>
2024-02-19 13:39:45 -0500

    feat: add reverseString function

--- functions.js
+++ functions.js
@@ -3,6 +3,18 @@ function capatilize(value) {
   return stringToCapatalize[0].toUpperCase() + stringToCapatalize.substring(1);
 }
 
+function reverseString(value) {
+  const stringToReverse = value.trim();
+  let reversedString = '';
+  let i = stringToReverse.length - 1;
+  while (i >= 0) {
+    reversedString += stringToReverse[i];
+    i -= 1;
+  }
+  return reversedString;
+}
+
 module.exports = {
   capatilize,
+  reverseString,
 };
