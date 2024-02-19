197f69f95a17fa3c36121398e775729b3802b897
Edward Rodriguez <36542201+Edward-Rodriguez@users.noreply.github.com>
2024-02-19 14:11:24 -0500

    test: add unit tests for calculator object add function

--- functions.test.js
+++ functions.test.js
@@ -1,6 +1,6 @@
 const myModule = require('./functions');
 
-const { capatilize, reverseString } = myModule;
+const { capatilize, reverseString, Calculator } = myModule;
 
 test('Capitalize', () => {
   expect(capatilize('odin')).toBe('Odin');
@@ -28,4 +28,20 @@ test('Reverse string with multiple words', () => {
 
 test('Reverse string spaces at both ends', () => {
   expect(reverseString('  moon shine  ')).toBe('enihs noom');
+});
+
+test('Add two numbers', () => {
+  expect(Calculator.add(1, 2)).toBe(3);
+});
+
+test('Add two floating point numbers', () => {
+  expect(Calculator.add(0.01, 0.02)).toBeCloseTo(0.03);
+});
+
+test('Add integer and float', () => {
+  expect(Calculator.add(1, 0.02)).toBeCloseTo(1.02);
+});
+
+test('Add integer and alpha-string', () => {
+  expect(Calculator.add(1, 'abc')).toBeNaN();
 });
