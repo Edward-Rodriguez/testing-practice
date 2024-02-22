b852f9c406da7f04413877a671502e610d2fc399
Edward Rodriguez <36542201+Edward-Rodriguez@users.noreply.github.com>
2024-02-22 12:50:52 -0500

    feat: add ceasarCipher function

--- functions.js
+++ functions.js
@@ -43,8 +43,28 @@ const Calculator = (() => {
   };
 })();
 
+/**
+ * alphapet used is ASCII values 32(Space) till 126(~ tilde)
+ */
+function ceasarCipher(string, shiftFactor) {
+  const startAscii = 32;
+  const endAscii = 126;
+  const stringToCipher = string;
+  let encryptedString = '';
+  for (let i = 0; i < stringToCipher.length; i += 1) {
+    let asciiValue = stringToCipher[i].charCodeAt(0);
+    asciiValue = (asciiValue + shiftFactor) % endAscii;
+    if (asciiValue < startAscii) {
+      asciiValue += startAscii - 1;
+    }
+    encryptedString += String.fromCharCode(asciiValue);
+  }
+  return encryptedString;
+}
+
 module.exports = {
   capatilize,
   reverseString,
   Calculator,
+  ceasarCipher,
 };
